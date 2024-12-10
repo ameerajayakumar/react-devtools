9b638a8ec0e8efd0ab9e7f71222c5299cfa89594
Ameera Jayakumar <ameeraj.mec@gmail.com>
2024-12-10 15:44:36 +0200

    test

--- src/todoStore.js
+++ src/todoStore.js
@@ -1,16 +1,16 @@
 // import { createStore } from "redux";
 // import { rootReducer } from "./reducers";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 // import { reducersMapping } from "./reducers";
-import { reducersMapping } from "./slices";
-import preferencesSlice from "./slices/preferencesSlice";
-import todoSlice from "./slices/todoSlice";
+import { reducersMapping } from './slices';
+import preferencesSlice from './slices/preferencesSlice';
+import todoSlice from './slices/todoSlice';
+//test-2reduxdev
 
-  
 // const store = createStore(rootReducer);
 const store = configureStore({
-    reducer: reducersMapping
-})
+  reducer: reducersMapping,
+});
 console.log(store);
 
 export default store;
